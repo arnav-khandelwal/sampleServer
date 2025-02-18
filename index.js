0c28ce51a5a511a40529996ef1eeefaf4245488e
Arnav Khandelwal <arnav181104@gmail.com>
2025-02-18 10:47:15 +0530

    added error handelling and comments

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = 8000;
 
 app.use(express.urlencoded({extended : false}));
 
+// main page
+app.get("/", (req, res) => {
+    res.send("hi i am arnav");
+});
+
+// get all users
 app.get("/users", (req, res) => {
     const HTML = `
         <ul>
@@ -15,10 +21,8 @@ app.get("/users", (req, res) => {
     res.send(HTML);
 });
 
-app.get("/", (req, res) => {
-    res.send("hi i am arnav");
-});
 
+// api end points 
 app.route("/api/users").get((req, res) => {
     return res.json(users);
 }).post((req, res) => {
@@ -32,11 +36,13 @@ app.route("/api/users").get((req, res) => {
 app.route("/api/users/:id").get((req, res) => {
     const ID = Number(req.params.id);
     const user = users.find((user) => user.id === ID);
+    if(!user) return res.json({status: "failed", message: `no user found with id ${ID}`});
     return res.json(user);
 }).patch((req, res) => {
     const body = req.body;
     const ID = Number(req.params.id);
     const user = users.find((user) => user.id === ID);
+    if(!user) return res.json({status: "failed", message: `no user found with id ${ID}`});
     Object.assign(user, body);
     return res.json({status: "success", newBody: user});
 
@@ -45,6 +51,8 @@ app.route("/api/users/:id").get((req, res) => {
     const index = users.findIndex((user) => user.id === ID);
     if (index !== -1) {
     users.splice(index, 1);
+    } else {
+        return res.json({status: "failed", message: `no user found with id ${ID}`});
     }
     return res.json({status: "success", remaining: users.length});
 });
